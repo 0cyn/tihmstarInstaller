9d259316f9f450e06b4959a13e9047d09d2ee6a3
Brandon Plank <52553007+BrandonPlank@users.noreply.github.com>
2019-12-31 14:52:57 -0500

    options + added ra1nsn0w

--- proj/tihmstarInstaller/main.m
+++ proj/tihmstarInstaller/main.m
@@ -44,121 +44,176 @@ void cdAndCompile(char *where){
     system([command UTF8String]);
 }
 
+void help(){
+    printf("Usage: tihmstarinstaller <arg>\n");
+    printf("-c              Just download and compile all the software\n");
+    printf("-d              Download all depends\n");
+    printf("-a              Do all\n");
+    printf("-h              Shows this help\n");
+    exit(0);
+}
 
 
-int main() {
+void downloadAndCompile(){
+    initFolerAndCD();
+    gitCloneRec("https://github.com/tihmstar/libgeneral");
+    gitCloneRec("https://github.com/tihmstar/img4tool");
+    gitCloneRec("https://github.com/tihmstar/liboffsetfinder64");
+    gitCloneRec("https://github.com/BrandonPlank/libipatcher");
+    gitCloneRec("https://github.com/tihmstar/libfragmentzip");
+    gitCloneRec("https://github.com/libimobiledevice/libirecovery");
+    gitCloneRec("https://github.com/tihmstar/libplist");
+    gitCloneRec("https://github.com/tihmstar/ra1nsn0w");
+    cdAndCompile("libgeneral");
+    cdAndCompile("img4tool");
+    cdAndCompile("liboffsetfinder64");
+    cdAndCompile("libipatcher");
+    cdAndCompile("libfragmentzip");
+    cdAndCompile("libirecovery");
+    cdAndCompile("libplist");
+    cdAndCompile("ra1nsn0w");
+    
+}
+
+void installDep(){
+    printf("Installing! Please Wait!\n");
+    InstallDepends("ack");
+    InstallDepends("atk");
+    InstallDepends("autoconf");
+    InstallDepends("automake");
+    InstallDepends("binutils");
+    InstallDepends("binwalk");
+    InstallDepends("boost");
+    InstallDepends("cairo");
+    InstallDepends("cifer");
+    InstallDepends("clutter");
+    InstallDepends("cmake");
+    InstallDepends("cogl");
+    InstallDepends("colormake");
+    InstallDepends("coreutils");
+    InstallDepends("cryptopp");
+    InstallDepends("curl");
+    InstallDepends("dex2jar");
+    InstallDepends("dns2tcp");
+    InstallDepends("docbook");
+    InstallDepends("docbook-xsl");
+    InstallDepends("dpkg");
+    InstallDepends("expat");
+    InstallDepends("fcrackzip");
+    InstallDepends("findutils");
+    InstallDepends("fontconfig");
+    InstallDepends("foremost");
+    InstallDepends("freetype");
+    InstallDepends("fribidi");
+    InstallDepends("gcc");
+    InstallDepends("gdbm");
+    InstallDepends("gdk-pixbuf");
+    InstallDepends("gettext");
+    InstallDepends("git");
+    InstallDepends("glib");
+    InstallDepends("gmp");
+    InstallDepends("gnu-tar");
+    InstallDepends("graphite2");
+    InstallDepends("gtk-doc");
+    InstallDepends("harfbuzz");
+    InstallDepends("hashpump");
+    InstallDepends("hydra");
+    InstallDepends("icu4c");
+    InstallDepends("isl");
+    InstallDepends("john");
+    InstallDepends("jpeg");
+    InstallDepends("json-glib");
+    InstallDepends("knock");
+    InstallDepends("ldid");
+    InstallDepends("libdnet");
+    InstallDepends("libffi");
+    InstallDepends("libimobiledevice");
+    InstallDepends("libmpc");
+    InstallDepends("libplist");
+    InstallDepends("libpng");
+    InstallDepends("libssh");
+    InstallDepends("libtasn1");
+    InstallDepends("libtiff");
+    InstallDepends("libtool");
+    InstallDepends("libusb");
+    InstallDepends("libusbmuxd");
+    InstallDepends("libxml2");
+    InstallDepends("libzip");
+    InstallDepends("lua");
+    InstallDepends("lynx");
+    InstallDepends("lzo");
+    InstallDepends("m4");
+    InstallDepends("moreutils");
+    InstallDepends("mpfr");
+    InstallDepends("mysql-client");
+    InstallDepends("nmap");
+    InstallDepends("node");
+    InstallDepends("openssl@1.1");
+    InstallDepends("p7zip");
+    InstallDepends("pango");
+    InstallDepends("pcre");
+    InstallDepends("pcre2");
+    InstallDepends("perl");
+    InstallDepends("pigz");
+    InstallDepends("pixman");
+    InstallDepends("pkg-config");
+    InstallDepends("pngcheck");
+    InstallDepends("python3");
+    InstallDepends("readline");
+    InstallDepends("screenfetch");
+    InstallDepends("socat");
+    InstallDepends("source-highlight");
+    InstallDepends("sqlite");
+    InstallDepends("sqlmap");
+    InstallDepends("ssdeep");
+    InstallDepends("tcpflow");
+    InstallDepends("tcpreplay");
+    InstallDepends("tcptrace");
+    InstallDepends("ucspi-tcp");
+    InstallDepends("xz");
+}
+
+
+void all(){
+    installDep();
+    downloadAndCompile();
+}
+
+int main(int argc, const char * argv[]) {
     @autoreleasepool {
         printf("===========================================\n");
         printf("Tihmstar depends installer by Brandon Plank\n");
-        printf("===========================================\n");
-        printf("Installing! Please Wait!\n");
-        InstallDepends("ack");
-        InstallDepends("atk");
-        InstallDepends("autoconf");
-        InstallDepends("automake");
-        InstallDepends("binutils");
-        InstallDepends("binwalk");
-        InstallDepends("boost");
-        InstallDepends("cairo");
-        InstallDepends("cifer");
-        InstallDepends("clutter");
-        InstallDepends("cmake");
-        InstallDepends("cogl");
-        InstallDepends("colormake");
-        InstallDepends("coreutils");
-        InstallDepends("cryptopp");
-        InstallDepends("curl");
-        InstallDepends("dex2jar");
-        InstallDepends("dns2tcp");
-        InstallDepends("docbook");
-        InstallDepends("docbook-xsl");
-        InstallDepends("dpkg");
-        InstallDepends("expat");
-        InstallDepends("fcrackzip");
-        InstallDepends("findutils");
-        InstallDepends("fontconfig");
-        InstallDepends("foremost");
-        InstallDepends("freetype");
-        InstallDepends("fribidi");
-        InstallDepends("gcc");
-        InstallDepends("gdbm");
-        InstallDepends("gdk-pixbuf");
-        InstallDepends("gettext");
-        InstallDepends("git");
-        InstallDepends("glib");
-        InstallDepends("gmp");
-        InstallDepends("gnu-tar");
-        InstallDepends("graphite2");
-        InstallDepends("gtk-doc");
-        InstallDepends("harfbuzz");
-        InstallDepends("hashpump");
-        InstallDepends("hydra");
-        InstallDepends("icu4c");
-        InstallDepends("isl");
-        InstallDepends("john");
-        InstallDepends("jpeg");
-        InstallDepends("json-glib");
-        InstallDepends("knock");
-        InstallDepends("ldid");
-        InstallDepends("libdnet");
-        InstallDepends("libffi");
-        InstallDepends("libimobiledevice");
-        InstallDepends("libmpc");
-        InstallDepends("libplist");
-        InstallDepends("libpng");
-        InstallDepends("libssh");
-        InstallDepends("libtasn1");
-        InstallDepends("libtiff");
-        InstallDepends("libtool");
-        InstallDepends("libusb");
-        InstallDepends("libusbmuxd");
-        InstallDepends("libxml2");
-        InstallDepends("libzip");
-        InstallDepends("lua");
-        InstallDepends("lynx");
-        InstallDepends("lzo");
-        InstallDepends("m4");
-        InstallDepends("moreutils");
-        InstallDepends("mpfr");
-        InstallDepends("mysql-client");
-        InstallDepends("nmap");
-        InstallDepends("node");
-        InstallDepends("openssl@1.1");
-        InstallDepends("p7zip");
-        InstallDepends("pango");
-        InstallDepends("pcre");
-        InstallDepends("pcre2");
-        InstallDepends("perl");
-        InstallDepends("pigz");
-        InstallDepends("pixman");
-        InstallDepends("pkg-config");
-        InstallDepends("pngcheck");
-        InstallDepends("python3");
-        InstallDepends("readline");
-        InstallDepends("screenfetch");
-        InstallDepends("socat");
-        InstallDepends("source-highlight");
-        InstallDepends("sqlite");
-        InstallDepends("sqlmap");
-        InstallDepends("ssdeep");
-        InstallDepends("tcpflow");
-        InstallDepends("tcpreplay");
-        InstallDepends("tcptrace");
-        InstallDepends("ucspi-tcp");
-        InstallDepends("xz");
-        initFolerAndCD();
-        gitCloneRec("https://github.com/tihmstar/libgeneral");
-        gitCloneRec("https://github.com/tihmstar/img4tool");
-        gitCloneRec("https://github.com/tihmstar/liboffsetfinder64");
-        gitCloneRec("https://github.com/BrandonPlank/libipatcher");
-        gitCloneRec("https://github.com/tihmstar/libfragmentzip");
-        gitCloneRec("https://github.com/libimobiledevice/libirecovery");
-        cdAndCompile("libgeneral");
-        cdAndCompile("img4tool");
-        cdAndCompile("liboffsetfinder64");
-        cdAndCompile("libipatcher");
-        cdAndCompile("libfragmentzip");
-        cdAndCompile("libirecovery");
+        printf("===========================================\n\n");
+        
+        
+        if (argc!=2){
+            // output usage
+            help();
+        
+            // leave with code 1
+            exit(1);
+        }
+        
+        
+        NSString *arg1 = [NSString stringWithUTF8String:argv[1]];
+        
+        if ([arg1  isEqual:@"-c"]){
+            
+            downloadAndCompile();
+            
+        }
+        else if ([arg1 isEqual:@"-a"]){
+            all();
+        }
+        else if ([arg1 isEqual:@"-d"]){
+            installDep();
+        }
+        else if ([arg1 isEqual:@"-h"]){
+            help();
+        }else{
+            help();
+        }
+
         printf("Fully installed!\nYou no-longer need to run this script!\n");
         printf("Now to compile any other tool, run\n./autogen.sh\nmake\nsudo make install\n");
     }
